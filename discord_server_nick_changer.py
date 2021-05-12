3aba6008cf175f8c39b89864432ac7b92d6dc65c
lilmond <54730301+lilmond@users.noreply.github.com>
2021-05-12 09:29:38 +0800

    Create discord_server_nick_changer.py

---
+++ discord_server_nick_changer.py
@@ -0,0 +1,21 @@
+import requests
+import time
+
+token = "authorization token here"
+server_id = input("Server ID: ")
+member_id = input("Member ID: ")
+url = f"https://discord.com/api/v9/guilds/{server_id}/members/{member_id}"
+texts = ["test1", "test2", "test3"]
+
+while True:
+    for text in texts:
+        data = {
+            "nick": text
+        }
+        headers = {
+            "authorization": token
+        }
+        r = requests.patch(url, json=data, headers=headers)
+        print(r.text)
+        print(r.status_code)
+        time.sleep(2)
