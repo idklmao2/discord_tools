c16f34250299f416698d88d2dd2c116c51bf946c
lilmond <54730301+lilmond@users.noreply.github.com>
2021-05-14 10:23:48 -0700

    Create discord_create_channel_spammer.py

---
+++ discord_create_channel_spammer.py
@@ -0,0 +1,23 @@
+import requests
+import json
+import time
+
+token = ""
+headers = {
+    "authorization": token
+}
+guild_id = 1234
+url = f"https://discord.com/api/v9/guilds/{guild_id}/channels"
+r = requests.get(url, headers=headers)
+channels = json.loads(r.text)
+for channel in channels:
+    if channel["name"] == "josh poop":
+        for _ in range(10):
+            data = {
+                "name": "josh poop",
+                "parent_id": channel["id"],
+                "permission_overwrites": [],
+                "type": "0"
+            }
+            requests.post(url, headers=headers, json=data)
+            time.sleep(1)
