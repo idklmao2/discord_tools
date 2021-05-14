7f1f2bd5fc4b632f01acb88ce46b02f1f5708952
lilmond <54730301+lilmond@users.noreply.github.com>
2021-05-14 07:57:36 -0700

    Create discord_channel_messages_fetcher.py

---
+++ discord_channel_messages_fetcher.py
@@ -0,0 +1,29 @@
+#Version = 1.0.0
+#Expect me to create another version of this piece of code.
+import requests
+import json
+
+token = ""
+headers = {
+    "authorization": token
+}
+
+def get_messages(channel_id, skipfrom=None):
+    fetched = 0
+    lastid = None
+    while True:
+        if fetched == 0 and startfrom != None:
+            url = f"https://discord.com/api/v9/channels/{channel_id}/messages?limit=50"
+        else:
+            url = f"https://discord.com/api/v9/channels/{channel_id}/messages?before={lastid}&limit=50"
+        response_text = requests.get(url, headers=headers).text
+        response_json = json.loads(response_text)
+        fetched += len(response_json)
+        if len(response_json) == 0:
+            break
+        print(len(response_json))
+        for message in response_json:
+            lastid = message["id"]
+            print(message["content"])
+
+get_messages(channel id here, message id here)
