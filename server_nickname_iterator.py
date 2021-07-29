d822924d5662d700c500bd0d351f49d0a0271f58
lilmond <54730301+lilmond@users.noreply.github.com>
2021-07-29 10:11:40 +0000

    Update server_nickname_iterator.py

--- server_nickname_iterator.py
+++ server_nickname_iterator.py
@@ -9,20 +9,27 @@ headers = {
     "Authorization": token
 }
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-context = ssl.create_default_context()
-s.connect(("discord.com", 443))
-s = context.wrap_socket(s, server_hostname="discord.com")
+def createSocket():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    context = ssl.create_default_context()
+    s.connect(("discord.com", 443))
+    s = context.wrap_socket(s, server_hostname="discord.com")
+    return s
+
+s = createSocket()
 
 def sendHttp(method, path="/", data=None):
     try:
+        global s
         if not path.startswith("/"):
             path = "/" + path
         http = f"{method} {path} HTTP/1.1\r\nHost: discord.com\r\nAuthorization: {token}\r\nContent-Type: application/json\r\nContent-Length: {len(json.dumps(data))}\r\n\r\n"
         http += json.dumps(data)
         s.send(http.encode())
     except Exception as e:
-        print(f"sendHttp error: {e}")
+        print(f"sendHttp error: {e}. Recreating socket...")
+        del s
+        s = createSocket()
         pass
 
 def changeNick(guild_id, nickname):
@@ -41,7 +48,7 @@ def main():
             for i in nickname:
                 current_nick += i
                 changeNick(guild_id, current_nick)
-                time.sleep(1)
+                time.sleep(3)
     except KeyboardInterrupt:
         sys.exit()
 
