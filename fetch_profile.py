441e8c35e563c348e10873995c3163164e3e3b63
lilmond <54730301+lilmond@users.noreply.github.com>
2021-08-02 19:48:58 +0000

    Update fetch_profile.py

--- fetch_profile.py
+++ fetch_profile.py
@@ -3,6 +3,7 @@ import json
 import sys
 
 token = ""
+image_size = 1024
 headers = {
     "Authorization": token
 }
@@ -19,13 +20,13 @@ def getprofile(user_id):
         avatar = profile["avatar"]
         discriminator = profile["discriminator"]
         user_tag = f"{username}#{discriminator}"
-        avatar_url = f"https://cdn.discordapp.com/avatars/{user_id}/{avatar}"
+        avatar_url = f"https://cdn.discordapp.com/avatars/{user_id}/{avatar}?size={image_size}"
         avatar_text = ""
         banner = ""
         if not profile["avatar"] == None:
             avatar_text = f"\r\nAvatar URL: {avatar_url}"
         if not profile["banner"] == None:
-            banner = f"\r\nBanner: https://cdn.discordapp.com/banners/{user_id}/{profile['banner']}"
+            banner = f"\r\nBanner: https://cdn.discordapp.com/banners/{user_id}/{profile['banner']}?size={image_size}"
         print(f"Tag: {user_tag}{avatar_text}{banner}\r\n\r\n")
     except Exception as e:
         print(f"getprofile error: {e}")
