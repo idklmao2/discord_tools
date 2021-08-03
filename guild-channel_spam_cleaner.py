5ac7e455994865947f49af857b6f48962b9248b8
lilmond <54730301+lilmond@users.noreply.github.com>
2021-08-03 04:26:51 +0000

    Update and rename discord_channel_spam_cleaner.py to guild-channel_spam_cleaner.py

--- guild-channel_spam_cleaner.py
+++ guild-channel_spam_cleaner.py
@@ -1,17 +1,20 @@
+# This is used to delete specific channels or categories by name in a server
+
 from time import time
 import requests
 import json
 import time
 
 token = ""
-guild_id = ""
+guild_id = input("Guild ID: ")
+channel_name = input("Channel Name: ")
 headers = {
     "authorization": token
 }
 r = requests.get(f"https://discord.com/api/v9/guilds/{guild_id}/channels", headers=headers)
 response_json = json.loads(r.text)
 for channel in response_json:
-    if channel["name"] == "josh-poop":
+    if channel["name"] == channel_name:
         d = requests.delete(f"https://discord.com/api/v9/channels/{channel['id']}", headers=headers)
         print(d.text)
         #time.sleep(1)
