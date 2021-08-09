84088020d8342d9313f23851a44c0d853678118d
lilmond <54730301+lilmond@users.noreply.github.com>
2021-08-09 03:05:44 +0000

    Create channel-ascii_spammer.py

---
+++ channel-ascii_spammer.py
@@ -0,0 +1,48 @@
+import threading
+import requests
+import random
+import time
+
+try:
+    token_file = open("token.txt", "r")
+    token = token_file.read().strip()
+    token_file.close()
+except FileNotFoundError:
+    token = input("Token: ")
+    token_file = open("token.txt", "w");
+    token_file.write(token)
+    token_file.close()
+    print(f"Token Saved: token.txt")
+except Exception:
+    pass
+headers = {
+    "Authorization": token
+}
+
+def message(channel_id, message):
+    try:
+        requests.post(f"https://discord.com/api/v9/channels/{channel_id}/messages", json={"content": message}, headers=headers)
+    except Exception as e:
+        print(f"message error: {e}")
+        pass
+
+def genchars(length):
+    try:
+        text = ""
+        for _ in range(length):
+            if len(text) == length:
+                break
+            text += "@everyone"
+            for _ in range(5):
+                number = random.randrange(13000)
+                text += chr(number)
+        return text
+    except Exception:
+        print(f"genchars error: {e}")
+        pass
+
+channel_id = input("Channel ID: ")
+while True:
+    for _ in range(10):
+        threading.Thread(target=message, args=[channel_id, genchars(2000)[:2000]]).start()
+    time.sleep(15)
